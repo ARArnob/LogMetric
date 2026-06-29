657659a1da7d5d58adf98e402b3e141c8b272f2a
Arnob <ararnob1002@gmail.com>
2026-06-29 23:51:23 +0600

    chore: isolate infrastructure secrets via environment variables

--- src/main/java/org/example/logmetricapi/config/ElasticsearchConfig.java
+++ src/main/java/org/example/logmetricapi/config/ElasticsearchConfig.java
@@ -16,6 +16,7 @@
 
 package org.example.logmetricapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
@@ -25,16 +26,20 @@ import org.springframework.data.elasticsearch.support.HttpHeaders;
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "org.example.logmetricapi")
 public class ElasticsearchConfig extends ElasticsearchConfiguration {
+    @Value("${ELASTIC_API_KEY}")
+    private String elasticApiKey;
 
+    @Value("${ELASTIC_URL}")
+    private String elasticUrl;
     @Override
     public ClientConfiguration clientConfiguration(){
         HttpHeaders headers = new HttpHeaders();
         // PASTE YOUR REAL COPY-PASTED TOKEN STRING HERE:
-        headers.add("Authorization", "ApiKey M2FIX0U1OEJOcXpCb2h1bHN2Tjk6WFdUOHgyZWhLVDhCTEw3Smd2ZDlBQQ==");
+        headers.add("Authorization", "ApiKey " + elasticApiKey);
 
         return ClientConfiguration.builder()
                 // PASTE YOUR REAL COPIED ENDPOINT URL HERE (Without https://, ending in :443):
-                .connectedTo("my-elasticsearch-project-bf1901.es.us-central1.gcp.elastic.cloud:443")
+                .connectedTo(elasticUrl)
                 .usingSsl()
                 .withDefaultHeaders(headers)
                 .build();
